47726b41edf4c52452419af9a67c5dd96a30e974
Michael Copeland <mcopeland2001@hotmail.com>
2020-10-26 22:08:58 +0900

    Reorganized tex files into many subdirectories

---
+++ tex/build.cmd
@@ -0,0 +1,7 @@
+@echo on
+
+pdflatex -synctex=1 -interaction=nonstopmode --output-directory=..\build "Pure Mathematics.tex"
+copy "..\build\Pure Mathematics.pdf" "..\pdf\Pure Mathematics.pdf"
+
+CALL search "vector spaces"
+copy "..\build\vector spaces.pdf" "..\pdf\vector spaces.pdf"
